aa24443e9815b722d89e8a7bde2a9ad2783f5b99
Marcin-99 <57869445+Marcin-99@users.noreply.github.com>
2020-06-11 14:32:39 +0200

    Add files via upload

---
+++ detect-sequence/detect-sequence.service.ts
@@ -0,0 +1,19 @@
+export class DetectSequenceService {
+    public static detect(inputSeq: number[], searchedSeq: number[]): number {
+        let stack: number[] = [];
+        searchedSeq.reverse();
+
+        for (let i = inputSeq.length - 1; i >= 0; i--) {
+            if (inputSeq[i] == searchedSeq[stack.length]) stack.push(inputSeq[i]);
+            else stack.splice(0, stack.length);
+            if (stack.toString() == searchedSeq.toString()) return i;
+        }
+
+        return -1;
+    }
+}
+
+
+let exampleInput = [9, 1, 2, 5, 1, 2, 5, 2, 5, 1];
+let sequence = [2, 5];
+console.log(DetectSequenceService.detect(exampleInput, sequence));
